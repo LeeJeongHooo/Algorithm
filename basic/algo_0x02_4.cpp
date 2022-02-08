a5e3516bd1f80eff9d2f3a225f33f769b04a7fcb
leejeongho <jh9628771@gmail.com>
2022-02-09 01:24:45 +0900

    Create algo_0x02_4.cpp in basic

---
+++ basic/algo_0x02_4.cpp
@@ -0,0 +1,16 @@
+/*BAEKJOON 10869*/
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio;
+    cin.tie(0);
+    int a,b;
+    cin >> a >> b;
+    cout << a + b << "\n";
+    cout << a - b << "\n";
+    cout << a * b << "\n";
+    cout << a / b << "\n";
+    cout << a % b << "\n";
+    return 0;
+}
