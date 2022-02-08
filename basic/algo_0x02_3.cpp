4c62c499b494fac639d7eb84d79c9a4e8bd884ea
leejeongho <jh9628771@gmail.com>
2022-02-08 23:11:14 +0900

    Create algo_0x02_3.cpp in basic

---
+++ basic/algo_0x02_3.cpp
@@ -0,0 +1,13 @@
+/*BAEKJOON 10171*/
+
+/*역슬레쉬를 출력하기위해는 //를 써줘야 된다. */
+#include <iostream>
+using namespace std;
+int main(void) {
+    ios::sync_with_stdio(0);
+    cout << "\\    /\\" << '\n';
+    cout << " )  ( \')" << '\n';
+    cout << "(  /  )" << '\n';
+    cout << " \\(__)|";
+    return 0;
+}
