a5e93aedf7cbb3019c498c11d464ad42ff3073f1
leejeongho <jh9628771@gmail.com>
2022-02-10 15:56:37 +0900

    Add comments algo_0x02__10.cpp in basic

--- basic/algo_0x02__10.cpp
+++ basic/algo_0x02__10.cpp
@@ -1,7 +1,8 @@
 /*BAEKJOON 2576*/
 #include <bits/stdc++.h>
 using namespace std;
-
+// x & 1 과 x % 2 는 동일  두개의 비트가 모두 1이어야만 1로 반환 홀수인지 짝수인지 만약하는 연산자 
+// x >> 1(오른쪽 시프트) 과  x / 2 동일  오른쪽 shift는 나누기 2 왼쪽 shift는 곱하기 연산자
 int main(void) {
     ios::sync_with_stdio;
     cin.tie(0);
