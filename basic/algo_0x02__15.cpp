515c4dd63ba7f6b711688d64406aff038b1e3c65
leejeongho <jh9628771@gmail.com>
2022-02-11 16:20:27 +0900

    Create algo_0x02__15.cpp in basic

---
+++ basic/algo_0x02__15.cpp
@@ -0,0 +1,15 @@
+/*BAEKJOON 15552*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n,x,y;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> x >> y;
+        cout << x + y << "\n";
+    }
+}
