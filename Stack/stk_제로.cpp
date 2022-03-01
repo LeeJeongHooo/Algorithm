33c70f4fc7bc32ed0da7b9d59ad62e4b222869b0
leejeongho <jh9628771@gmail.com>
2022-03-02 00:06:19 +0900

    Update stk_제로.cpp in Stack

--- Stack/stk_제로.cpp
+++ Stack/stk_제로.cpp
@@ -6,4 +6,25 @@ using namespace std;
 int main(void){
     ios::sync_with_stdio(0);
     cin.tie(0);
+    
+    stack<int> s;
+    int K;
+    cin >> K;
+    for(int i = 0; i < K; i++){
+        int x;
+        cin >> x;
+        if(x == 0){
+            if(!s.empty())
+            s.pop();
+        }
+        else{
+            s.push(x);
+        }
+    }
+    int sum = 0;
+    while(!s.empty()){
+        sum += s.top();
+        s.pop();
+    }
+    cout << sum;
 }
