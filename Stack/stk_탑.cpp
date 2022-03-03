79a04519f114b11c1134e7f37d27654add9a185e
leejeongho <jh9628771@gmail.com>
2022-03-03 16:02:02 +0900

    Create stk_탑.cpp in Stack

--- Stack/stk_탑.cpp
+++ Stack/stk_탑.cpp
@@ -2,23 +2,34 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-#define F fisrt
-#define S second
 
+stack<pair<int, int>> tower; //스택을 한쌍으로 선언가능
 int main(void){
     ios::sync_with_stdio(0);
     cin.tie(0);
     int n;
     cin >> n;
-    stack<pair<int, int>> tower; //스택을 한쌍으로 선언가능
-    tower.push({10000000},0);
+
     for(int i = 1; i <= n; i++){
         int h;
         cin >> h;
-        while(tower.top().fisrt < h){
-            tower.pop();
+        if(tower.empty()){
+            tower.push({h, i});
+            cout << 0 << " ";
+            continue;
         }
-        cout << tower.top().second << " ";
+        while(tower.top().first < h){
+            tower.pop();
+            if(tower.empty()){
+                break;
+            }
+        }
+        if(tower.empty()){
+            cout << 0 << " ";
+        }
+        else{
+            cout << tower.top().second << " ";
+        }
         tower.push({h, i});
     }
 }
