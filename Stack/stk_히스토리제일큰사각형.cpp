827d5f9c899a64686069363927b7b7c649f25cc7
leejeongho <jh9628771@gmail.com>
2022-03-08 18:37:12 +0900

    Create incomplete stk_히스토리제일큰사각형.cpp in Stack

---
+++ Stack/stk_히스토리제일큰사각형.cpp
@@ -0,0 +1,15 @@
+/*BAEKJOON 6549*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin << n;
+
+    for(int i = 0; i < n; i++) {
+        int h;
+        cin >> h;
+    }
+}
