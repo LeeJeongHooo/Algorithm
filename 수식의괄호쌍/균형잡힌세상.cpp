b512ddc5615a46e9db309c61bc537044423bcb3e
leejeongho <jh9628771@gmail.com>
2022-04-04 23:42:27 +0900

    Update comment 균형잡힌세상.cpp in 수식의괄호쌍

--- 수식의괄호쌍/균형잡힌세상.cpp
+++ 수식의괄호쌍/균형잡힌세상.cpp
@@ -1,36 +1,58 @@
 /* BAEKJOON  4949 */
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main(void)
+{
   ios::sync_with_stdio(0);
   cin.tie(0);
-  while(true){
+  while (true)
+  {
     string s;
-    getline(cin, s); //공백인 문자열을 입력받기 위해 getline함수를 사용
-    if(s == ".") break;
     stack<char> stk;
-    bool isValid = true;
-    for(auto c : s){
-      if(c == '(' || c == '[') stk.push(c);
-      else if(c == ')'){
-        if(stk.empty() || stk.top() != '('){
-          isValid = false;
-          break;
-        }
-        stk.pop();
+    bool check = true;
+    getline(cin, s);
+    if (s == ".") //입력이 .일경우 실행 종료
+    {
+      break;
+    }
+    for (auto c : s) // c++11이상부터지원가능 s의 문자 하나하나 읽어오기
+    {
+      if (c == '[' || c == '(')
+      {
+        stk.push(c);
       }
-      else if(c == ']'){
-        if(stk.empty() || stk.top() != '['){
-          isValid = false;
+      else if (c == ']')
+      {
+        if (stk.empty() || stk.top() != '[') //주의사항 stk가 비워있는 경우 top을 할때 런타임에러가 발생하므로 empty를 먼저확인
+        {
+          check = false;
           break;
         }
-        stk.pop();
+        else stk.pop();
+      }
+      else if (c == ')')
+      {
+        if (stk.empty() || stk.top() != '(')
+        {
+          check = false;
+          break;
+        }
+        else stk.pop();
       }
     }
-    if(!stk.empty()) isValid = false;
-    if(isValid) cout << "yes\n";
-    else cout << "no\n";
+    if (!stk.empty())
+    {
+      check = false;
+    }
+    if (check) //괄호쌍이 맞는 경우
+    {
+      cout << "yes" << "\n";
+    }
+    else //괄호쌍 규칙이 어긋난 경우
+    {
+      cout << "no" << "\n";
+    }
   }
 }
